98e0b51a258dcd91a770a75dc885edb40ee40240
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-24 13:24:11 +0100

    added margin to buttons, homework done

--- react-homework/src/Button.js
+++ react-homework/src/Button.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import RaisedButton from 'material-ui/RaisedButton'
 
+const style = {
+    margin: 10,
+}
+
 const Button = (props) => (
     <div>
         <RaisedButton
             label={props.label}
             primary={true}
             onClick={props.onClick}
+            style={style}
 
         />
     </div>
