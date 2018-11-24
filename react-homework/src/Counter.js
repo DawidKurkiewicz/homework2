056e6921073c3a4f90d191c0c1af2e121c20a1aa
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-24 12:33:53 +0100

    added 2 more functions

--- react-homework/src/Counter.js
+++ react-homework/src/Counter.js
@@ -4,11 +4,17 @@ class Counter extends React.Component {
     state = {
         number: this.props.startNumber || 0
     }
-    pPlusOneHandler = () => {
+    plusOneHandler = () => {
         this.setState({ number: this.state.number + 1 })
+    }
+    minusOneHandler = () => {
+        this.setState({ number: this.state.number - 1 })
     }
     plusFiveHandler = () => {
         this.setState({ number: this.state.number + 5 })
+    }
+    minusFiveHandler = () => {
+        this.setState({ number: this.state.number - 5 })
     }
     zeroHandler = () => {
         this.setState({ number: 0})
